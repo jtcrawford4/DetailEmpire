0940ec8f7da39a76cf316774e1af9b279c5ffa72
johncrawford <jtcrawford4@gmail.com>
2024-08-29 12:33:10 -0500

    Initial Commit

---
+++ DetailEmpire/DetailEmpireApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetailEmpireApp.swift
+//  DetailEmpire
+//
+//  Created by John Crawford on 8/29/24.
+//
+
+import SwiftUI
+
+@main
+struct DetailEmpireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
